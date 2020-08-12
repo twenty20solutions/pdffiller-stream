fed6e11ce796cb29dd7762f5c18d7b6a0540567c
Kyle McNally <kyle@kmcnally.net>
2020-08-12 08:29:26 -0400

    File Exists (#12)
    This will check to see if the source pdf exists before continuing

--- test/test.js
+++ test/test.js
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /*
  *   File:       pdf.js
  *   Project:    PDF Filler
@@ -48,16 +52,19 @@ test("should use toFile to create a completely filled PDF that is read-only", as
     t.is(roFdf.length, 0);
 });
 
-/*
-test("should use toFile to create a completely filled PDF, but to an invalid path", async (t) => {
+test("should throw when the sourcePDF doesn't exist", async (t) => {
+    const error = await t.throwsAsync(() =>
+        pdfFiller.fillForm("nope.pdf", data)
+    );
+    t.is(error.message, "File does not exist or is not readable");
+});
+
+test.skip("should throw when output is an invalid path", async (t) => {
     const error = await t.throwsAsync(async () => {
-        await pdfFiller
-            .fillFormWithFlatten(source2PDF, _data, true)
-            .toFile("/");
+        await pdfFiller.fillFormWithFlatten(source2PDF, data, true).toFile("/");
     });
     t.is(error, "Error: EISDIR: illegal operation on a directory, open '/'");
 });
-*/
 
 test("should create a FDF template with a null value", (t) => {
     const fdfData = createFdf({
@@ -67,12 +74,11 @@ test("should create a FDF template with a null value", (t) => {
     t.assert(fdfData);
 });
 
-/*
-test("should fail to FDF template with an invalid value", (t) => {
+test.skip("should fail to FDF template with an invalid value", (t) => {
     const fdfData = createFdf({
-        ..._data,
+        ...data,
         badval: {
-            badvar: function () {
+            badvar: () => {
                 return false;
             },
         },
@@ -80,7 +86,6 @@ test("should fail to FDF template with an invalid value", (t) => {
     console.log(fdfData.toString());
     t.not(fdfData, 0);
 });
-*/
 
 test("should create an unflattened PDF with unfilled fields remaining", async (t) => {
     const data2 = {
