772e10bdf1e2bc8f25e0644592acd44aa81dea60
Sparticuz <kyle@kmcnally.net>
2023-02-03 08:53:30 -0500

    fix ternary

--- src/convert-field-json-to-fdf.ts
+++ src/convert-field-json-to-fdf.ts
@@ -8,11 +8,7 @@ export default (formFields: FormField[]): Record<string, string> => {
   const fields: Record<string, string> = {};
   for (const row of formFields) {
     if (typeof row.fieldValue === "boolean") {
-      if (row.fieldValue) {
-        fields[row.title] = "Yes";
-      } else {
-        fields[row.title] = "Off";
-      }
+      fields[row.title] = row.fieldValue ? "Yes" : "Off";
     } else {
       fields[row.title] = row.fieldValue;
     }
